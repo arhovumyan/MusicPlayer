674ca8d2becb8e3e2735c769b88bbfe0eb8bfe20
Arego11 <aregar20012001@gmail.com>
2025-05-19 15:20:18 -0700

    allow the render url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -15,5 +14,6 @@ export default defineConfig({
   preview: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     host: true,
+    allowedHosts: ['musicplayer-9oht.onrender.com'],
   },
 })
