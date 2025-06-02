cb137a2bc63013eb9520c115da171707a50e605e
Arego11 <aregar20012001@gmail.com>
2025-06-02 15:27:50 -0700

    frontal page added songs for you, trending and featured

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import { getAllSongs, getFeaturedSongs ,getMadeForYouSongs, getTrendingSongs} from '../controllers/song.controller.js'
+import { protextRoute, requireAdmin } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send("Song route with GET method")
-})
+router.get('/', protextRoute, requireAdmin, getAllSongs)
+router.get('/featured', getFeaturedSongs)
+router.get('/made-for-you', getMadeForYouSongs)
+router.get('/trending', getTrendingSongs)
 
 export default router
